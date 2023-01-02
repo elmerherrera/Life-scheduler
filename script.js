21f11495c331a12c932c5989f315b78c5a856c1b
Elmer Herrera <dcshoeco109790@yahoo.com>
2023-01-02 18:38:48 -0500

    added the save feature in local storage

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
     for (var i = 0; i < 24; i++) {
         var hourNumber = moment(i, 'HH').format('h:mm a');
         $('#schedule').append(
@@ -20,5 +21,16 @@ $(document).ready(function(){
         } else {
             $(this).addClass('future');
         }
+    })
+    $('.save-button').click(function(){
+        var hourNumber = $(this).parent().find('.hour-number').text();
+        var hourText = $(this).parent().find('.hour-text').val();
+        localStorage.setItem(hourNumber, hourText);
+    })
+
+    $('.hour-text').each(function(){
+        var hourNumber = $(this).parent().find('.hour-number').text();
+        var hourText = localStorage.getItem(hourNumber);
+        $(this).val(hourText);
     })
 })
