43c1a99f0c30a49e3ed0781f12992a03ec544e4f
Elmer Herrera <dcshoeco109790@yahoo.com>
2023-01-02 18:14:02 -0500

    added and implented js for certain functions

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    for (var i = 0; i < 24; i++) {
+        var hourNumber = moment(i, 'HH').format('h:mm a');
+        $('#schedule').append(
+            '<div class="row hour">' +
+            '  <div class="col-1 hour-number">' + hourNumber + '</div>' +
+            '  <textarea class="col-10 hour-text"></textarea>' +
+            '  <button class="col-1 save-button">Save</button>' +
+            '</div>'
+        )
+    }
+
+    $('.hour-text').each(function() {
+        var hourNumber = $(this).parent().find('.hour-number').text();
+        var hourTime = moment(hourNumber, 'h:mm a');
+        if (hourTime.isBefore(moment(), 'hour')) {
+            $(this).addClass('past');
+        } else if (hourTime.isSame(moment(), 'hour')) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+    })
+})
